4e66fb94c100fdc59b2553f1a60714bc80b5cedf
Joel <dombekj890@gmail.com>
2024-07-08 05:24:26 -0400

    vue.js [stable]

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-	<div id="app">
-		<AppHeader /> <!-- Include the AppHeader component -->
-		<router-view /> <!-- This will render the current route component -->
-		<AppFooter /> <!-- Include the AppFooter component -->
-	</div>
-  </template>
-  
-  <script>
-  import AppHeader from './components/AppHeader.vue'; // Import the AppHeader component
-  import AppFooter from './components/AppFooter.vue'; // Import the AppFooter component
-  
-  export default {
-	name: 'App',
-	components: {
-		AppHeader, // Register the AppHeader component
-		AppFooter  // Register the AppFooter component
-	}
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-  </script>
-  
-  <style>
-  #app {
-	font-family: Arial, sans-serif;
-	margin: 20px;
+
+  .logo {
+    margin: 0 2rem 0 0;
   }
-  </style>
-  
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Import the router configuration
-
-createApp(App)
-    .use(router) // Use the router configuration
-    .mount('#app');
